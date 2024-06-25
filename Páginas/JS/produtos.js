231fe46c2ea86d1e80ca1fb66027f8b7c0a3ca7f
JohnStardust <166511710+JohnStardust@users.noreply.github.com>
2024-06-25 11:40:31 -0300

    Update produtos.js

--- Páginas/JS/produtos.js
+++ Páginas/JS/produtos.js
@@ -1,25 +1,4 @@
-// var caixa1 = document.getElementById('caixa1');
-// var caixa2 = document.getElementById('caixa2');
-// var caixa3 = document.getElementById('caixa3');
-// var caixa4 = document.getElementById('caixa4');
 
-// caixa1.addEventListener('click', caixa01)
-// caixa2.addEventListener('click', caixa02)
-// caixa3.addEventListener('click', caixa03)
-// caixa4.addEventListener('click', caixa04)
-
-// function caixa01(){
-    
-// }
-// function caixa02(){
-
-// }
-// function caixa03(){
-
-// }
-// function caixa04(){
-
-// }
 
 function abrirPagina(funcao) {
     window.location.href = `../HTML/pag_serviço.html?funcao=${funcao}`;

1cc61ee9e639530d7d86d885f930689b1512ab5a
Guvulcani <pedroaloia255@gmail.com>
2024-06-27 11:55:26 -0300

    sfAF

--- Páginas/JS/carrossel.js
+++ Páginas/JS/carrossel.js
@@ -4,11 +4,11 @@ let contador = 0;
 
 function slider(){
     contador++;
-    if(contador > imagens.length - 4){
+    if(contador > imagens.length - 8){
         contador = 0;
     }
 
-    box.style.transform = `translateX(${-contador * 100}%)`
+    box.style.transform = `translateX(${-contador * 1200}px)`
 }
 
 setInterval(slider, 3000);
